a088605baafe2d02a3f659f42f7060c9b7f627bb
Daniel Ferolino <dannydev@Daniels-MacBook-Pro.local>
2018-04-02 19:05:38 -0600

    presentation

--- app.js
+++ app.js
@@ -4,35 +4,50 @@ $(document).ready(function() {
     var output3 = $('#output3');
 
     $('#doSomething').on('click', function() {
-        $.ajax({
-            type: "GET",
-            url: "data/path1.json",
-            success: function(response) {
-                output1.html('<img src="' + response.path + '"/>');
-                $.ajax({
-                    type: "GET",
-                    url: "data/path2.json",
-                    success: function(response) {
-                        output2.html('<img src="' + response.path + '"/>');
-                        $.ajax({
-                            type: "GET",
-                            url: "data/path3.json",
-                            success: function(response) {
-                                output3.html('<img src="' + response.path + '"/>');
-                            },
-                            error: function(error) {
-                                console.log(error);
-                            }
-                        });
-                    },
-                    error: function(error) {
-                        console.log(error);
-                    }
-                });
-            },
-            error: function(error) {
-                console.log(error);
-            }
+        // $.ajax({
+        //     type: "GET",
+        //     url: "data/path1.json",
+        //     success: function(response) {
+        //         output1.html('<img src="' + response.path + '"/>');
+        //         $.ajax({
+        //             type: "GET",
+        //             url: "data/path2.json",
+        //             success: function(response) {
+        //                 output2.html('<img src="' + response.path + '"/>');
+        //                 $.ajax({
+        //                     type: "GET",
+        //                     url: "data/path3.json",
+        //                     success: function(response) {
+        //                         output3.html('<img src="' + response.path + '"/>');
+        //                     },
+        //                     error: function(error) {
+        //                         console.log(error);
+        //                     }
+        //                 });
+        //             },
+        //             error: function(error) {
+        //                 console.log(error);
+        //             }
+        //         });
+        //     },
+        //     error: function(error) {
+        //         console.log(error);
+        //     }
+        // });
+        $.get("data/path1.json").then(function(response) {
+            output1.html('<img src="' + response.path + '"/>');
+            return $.get("data/path2.json");
+        }).catch(function(error) {
+            console.log(error);
+        }).then(function(response) {
+            output2.html('<img src="' + response.path + '"/>');
+            return $.get("data/path3.json");
+        }).catch(function(error) {
+            console.log(error);
+        }).then(function(response) {
+            output3.html('<img src="' + response.path + '"/>');
+        }).catch(function(error) {
+            console.log(error);
         });
-    })
+    });
 });
